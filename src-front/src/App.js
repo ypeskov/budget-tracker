ab8ed322212d48a60ab8aa52e979a5eb98cbaaf7
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-06-15 14:55:32 +0300

    start working on front app

--- src-front/src/App.js
+++ src-front/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './App.css';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+      <Row>
+        <Col>
+        <header className="App-header">
+          Header
+        </header>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Login />
+        </Col>
+        <Col>
+          <Login />
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 }
