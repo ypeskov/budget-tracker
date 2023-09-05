507aa9c6cae43973a88991353adda4bc027d2787
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-09-05 20:27:07 +0300

    qqq

--- src-front/src/views/LoginView.vue
+++ src-front/src/views/LoginView.vue
@@ -1,31 +1,35 @@
 <script setup>
-  import { ref } from 'vue';
-  
-  const loginEmail = ref('');
-  const loginPassword = ref('');
+import { ref } from 'vue';
+import { useBudgetStore } from '../stores/budget';
 
-  const loginPath = 'http://localhost:9000/auth/login';
-  async function submitLogin() {
-    const requestBody = {
-      email: loginEmail.value,
-      password: loginPassword.value
-    };
-    const response = await fetch(loginPath, {
-      'method': 'POST',
-      'headers': {
-        'Content-Type': 'application/json'
-      },
-      'body': JSON.stringify(requestBody)
-    })
-  }
+const loginEmail = ref('');
+const loginPassword = ref('');
+
+const loginPath = 'http://localhost:9000/auth/login';
+async function submitLogin() {
+  const requestBody = {
+    email: loginEmail.value,
+    password: loginPassword.value
+  };
+  const response = await fetch(loginPath, {
+    'method': 'POST',
+    'headers': {
+      'Content-Type': 'application/json'
+    },
+    'body': JSON.stringify(requestBody)
+  });
+
+  const data = await response.json();
+  console.log(data);
+}
 </script>
 
 <template>
-    <div class="container">
-      <main>
-        <form @submit.prevent="submitLogin">
+  <div class="container">
+    <main>
+      <form @submit.prevent="submitLogin">
         <div class="row">
-          <div class="col">   
+          <div class="col">
             <input type="text" v-model="loginEmail" />
           </div>
         </div>
@@ -39,7 +43,7 @@
             <button>Login</button>
           </div>
         </div>
-        </form>
-      </main>
-    </div>
+      </form>
+    </main>
+  </div>
 </template>
