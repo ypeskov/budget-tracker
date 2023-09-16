75bea66b0da0f43db53d71f220dcd8561e16e04c
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-09-16 10:42:29 +0300

    Transaction model is changed to annotated mapped_column()

--- back-fastapi/app/models/Transaction.py
+++ back-fastapi/app/models/Transaction.py
@@ -1,8 +1,14 @@
-from sqlalchemy import Column, String, Integer, DateTime, func, Boolean, \
-    ForeignKey, Numeric
-from sqlalchemy.orm import relationship
+from decimal import Decimal
+from datetime import datetime
+
+from sqlalchemy import String, DateTime, func, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from app.database import Base
+from app.models.Account import Account
+from app.models.Currency import Currency
+from app.models.User import User
+from app.models.UserCategory import UserCategory
 
 LABEL_MAX_LENGTH = 50
 
@@ -10,34 +16,57 @@ LABEL_MAX_LENGTH = 50
 class Transaction(Base):
     __tablename__ = 'transactions'
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), index=True)
-    account_id = Column(Integer, ForeignKey('accounts.id'), index=True)
-    target_account_id = Column(Integer, ForeignKey('accounts.id'), index=True,
-                               nullable=True, default=None)
-    category_id = Column(Integer, ForeignKey('user_categories.id'), index=True)
-    currency_id = Column(Integer, ForeignKey('currencies.id'), index=True)
-    amount = Column(Numeric)
-    label = Column(String(LABEL_MAX_LENGTH), index=True)
-    notes = Column(String)
-    datetime = Column(DateTime(timezone=True), index=True)
-    exchange_rate = Column(Numeric)
-    is_transfer = Column(Boolean, nullable=False)
-    is_income = Column(Boolean, default=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), index=True)
+    account_id: Mapped[int] = mapped_column(ForeignKey('accounts.id'),
+                                            index=True)
+    target_account_id: Mapped[int] = mapped_column(ForeignKey('accounts.id'),
+                                                   index=True,
+                                                   nullable=True, default=None)
+    category_id: Mapped[int] = mapped_column(ForeignKey('user_categories.id'),
+                                             index=True)
+    currency_id: Mapped[int] = mapped_column(ForeignKey('currencies.id'),
+                                             index=True)
+    amount: Mapped[Decimal] = mapped_column()
+    label: Mapped[str] = mapped_column(String(LABEL_MAX_LENGTH), index=True)
+    notes: Mapped[str] = mapped_column()
+    datetime: Mapped[datetime] = mapped_column(DateTime(timezone=True),
+                                               index=True)
+    exchange_rate: Mapped[Decimal] = mapped_column()
+    is_transfer: Mapped[bool] = mapped_column(nullable=False)
+    is_income: Mapped[bool] = mapped_column(default=False)
 
-    user = relationship("User", back_populates='transactions')
+    user: Mapped[User] = relationship(back_populates='transactions')
 
-    account = relationship("Account", foreign_keys="Transaction.account_id")
+    account: Mapped[Account] = relationship(
+        foreign_keys="Transaction.account_id")
 
-    target_account = relationship("Account",
-                                  foreign_keys="Transaction.target_account_id")
+    target_account: Mapped[Account] = relationship(
+        foreign_keys="Transaction.target_account_id")
 
-    category = relationship("UserCategory")
-    currency = relationship("Currency")
+    category: Mapped[UserCategory] = relationship()
+    currency: Mapped[Currency] = relationship()
 
-    is_deleted = Column(Boolean, default=False, nullable=True,
-                        server_default='f')
-    created_at = Column(DateTime(timezone=True), server_default=func.now(),
-                        nullable=False)
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(),
-                        onupdate=func.now(), nullable=False)
+    is_deleted: Mapped[bool] = mapped_column(default=False, nullable=True,
+                                             server_default='f')
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),
+                                                 server_default=func.now(),
+                                                 nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),
+                                                 server_default=func.now(),
+                                                 onupdate=func.now(),
+                                                 nullable=False)
+
+    # def __str__(self):
+    #     return f'Transaction: id:{self.id}, user:{self.user.id},' + \
+    #         f' amount:{self.amount}{self.currency.code},' + \
+    #         f' account:{self.account.name}'
+
+    def __repr__(self):
+        return f'Transaction(id={self.id}, user_id={self.user_id}), ' + \
+            f'account_id={self.account_id}, target_account_id={self.target_account_id}, ' + \
+            f'category_id={self.category_id}, amount={self.amount}, ' + \
+            f'label={self.label}, notes={self.notes}, datetime={self.datetime}, ' + \
+            f'exchange_rate={self.exchange_rate}, is_transfer={self.is_transfer}, ' + \
+            f'is_income={self.is_income}, is_deleted={self.is_deleted}, ' + \
+            f'created_at={self.created_at}, updated_at={self.updated_at}'
