094b920ab6432715376f7f8e59bfd7cd4fd4c29c
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-11-27 16:27:59 +0200

    qqq

--- back-fastapi/app/config.py
+++ back-fastapi/app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 class Settings(BaseSettings):
     db_user: str = 'username'
     db_password: str = 'userpassword'
-    db_host: str = 'db'
+    db_host: str = 'db-budgeter'
     db_name: str = 'dbname'
 
     model_config = SettingsConfigDict(env_file=('.env', '.env.local', '.env.prod'))
