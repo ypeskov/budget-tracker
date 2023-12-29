dbfbb2ec3fcfe7466d0279c4f52f5363da3de555
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-12-29 13:47:38 +0200

    qqq

--- back-fastapi/run_tests.sh
+++ back-fastapi/run_tests.sh
@@ -2,10 +2,11 @@
 
 source venv/bin/activate
 
+OLD_PYTHONPATH=$PYTHONPATH
 export PYTHONPATH=$(pwd):$PYTHONPATH
-
 export TEST_MODE=True
 
 pytest --cov-report term-missing --cov=app app/tests/ -s
 
 unset TEST_MODE
+export PYTHONPATH=$OLD_PYTHONPATH
