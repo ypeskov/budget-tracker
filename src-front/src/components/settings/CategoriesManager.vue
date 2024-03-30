ac4b9f64dbe4222269f0fb58a1b39cb8583147dd
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-03-30 11:39:31 +0200

    Add categories component init

---
+++ src-front/src/components/settings/CategoriesManager.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  closeModal: Function,
+});
+
+
+</script>
+
+<template>
+  <div class="modal-overlay" @click="closeModal">
+    <div class="modal-content" @click.stop>
+      <h2>{{ $t('message.categories') }}</h2>
+      <div class="container">
+        <div class="row">
+          <button class="btn btn-secondary" @click="closeModal">{{ $t('buttons.close') }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
