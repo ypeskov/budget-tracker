60b8632e5a4ad8dbd3f9885c70d9a9d8f699a9d6
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-04-09 22:11:11 +0300

    Add created new logic for adding new transaction of non-transfer type

--- back-fastapi/app/routes/transations.py
+++ back-fastapi/app/routes/transations.py
@@ -28,13 +28,13 @@ def add_user_transaction(transaction_dto: CreateTransactionSchema, request: Requ
     try:
         transaction = create_transaction(transaction_dto, request.state.user['id'], db)
     except AccessDenied:
-        logger.exception(f'Access denied')
+        logger.error(f'Access denied')
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access denied")
     except InvalidCategory:
-        logger.exception(f'Invalid category: {transaction_dto}')
+        logger.error(f'Invalid category: {transaction_dto}')
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid category")
     except InvalidAccount:
-        logger.exception(f'Invalid account: {transaction_dto}')
+        logger.error(f'Invalid account: {transaction_dto}')
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Invalid account")
     except Exception as e:  # pragma: no cover
         logger.exception(e)
