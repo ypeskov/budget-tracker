8dc26cfc055ad9251e6f1d8d62cf24066bd0f95c
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-04-12 20:03:41 +0300

    refactoring of celery.py

---
+++ run_docker-compose.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+  echo "Starting services without back app..."
+  docker-compose up db-budgeter adminer-budgeter front redis-budgeter celery-budgeter celery-beat
+elif [ $1 == "full" ]; then
+  echo "Starting all services..."
+  docker-compose up
+else
+  echo "Unknown argument: $1"
+  echo "Variants:"
+  echo "  0 - default - Start all services without back app"
+  echo "  full - Start all services"
+  exit 1
+fi
