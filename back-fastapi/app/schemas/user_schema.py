f33daf7f4a456730f107f688ebb838fdbd6cadd0
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-04-14 08:38:51 +0300

    refactoring

--- back-fastapi/app/schemas/user_schema.py
+++ back-fastapi/app/schemas/user_schema.py
@@ -1,4 +1,5 @@
 from pydantic import ConfigDict, BaseModel, EmailStr, Field
+from pydantic.alias_generators import to_camel
 
 
 PASSWORD_MIN_LENGTH = 3
@@ -7,9 +8,9 @@ password_field: str = Field(min_length=PASSWORD_MIN_LENGTH, max_length=PASSWORD_
 
 
 class UserBase(BaseModel):
-    id: int | None = None
     email: EmailStr
-    model_config = ConfigDict(from_attributes=True)
+
+    model_config = ConfigDict(from_attributes=True, alias_generator=to_camel)
 
 
 class UserRegistration(UserBase):
@@ -23,6 +24,6 @@ class UserLoginSchema(UserBase):
 
 
 class UserResponse(UserBase):
+    id: int | None = None
     first_name: str | None = None
     last_name: str | None = None
-    model_config = ConfigDict(from_attributes=True)
