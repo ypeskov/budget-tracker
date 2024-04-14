81be188610f0e4553cb8eb111937fcf03668d0f6
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-04-14 10:44:04 +0300

    changes in exchange_rate model

--- back-fastapi/app/models/ExchangeRateHistory.py
+++ back-fastapi/app/models/ExchangeRateHistory.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import datetime,date
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Integer, DateTime, func, Boolean
+from sqlalchemy import Integer, DateTime, Date, func, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import JSONB
 
@@ -13,7 +13,7 @@ class ExchangeRateHistory(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     rates: Mapped[dict] = mapped_column(JSONB, nullable=True)
-    datetime: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
+    actual_date: Mapped[date] = mapped_column(Date(), index=True)
 
     is_deleted: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True, server_default='f')
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
