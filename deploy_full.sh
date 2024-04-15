71058c39da873e968399c0cfcffe33d0311139d5
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-04-16 01:52:44 +0300

    qqqq

--- deploy_full.sh
+++ deploy_full.sh
@@ -4,8 +4,8 @@ echo "Now pulling the latest changes from the repository."
 git pull
 
 echo "Pull the Docker images from Docker Hub and restart the containers in docker-compose.prod.yaml."
-docker-compose -f docker-compose.prod.yaml down &&\
-docker-compose up -f docker-compose.prod.yaml -d
+docker-compose -f docker-compose.prod.yml down &&\
+docker-compose up -f docker-compose.prod.yml -d
 
 echo "Building the front-end and restart front-end"
 cd src-front && bash build.sh
