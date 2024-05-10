9fd605cc3ae053273172c4857afa5d241f915822
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-05-10 19:09:23 +0300

    add caching of accounts list for 1 hour

--- src-front/src/stores/categories.js
+++ src-front/src/stores/categories.js
@@ -2,16 +2,14 @@ import { reactive, ref } from 'vue';
 import { defineStore } from 'pinia';
 import { DateTime } from 'luxon';
 
-export const useAccountStore = defineStore('account', () => {
-  const accounts = reactive([]);
+export const useCategoriesStore = defineStore('categories', () => {
+  const categories = reactive([]);
   const lastUpdated = reactive(DateTime.now());
   const shouldUpdate = ref(true);
-  const accountTypes = reactive([]);
 
   return {
-    accounts,
+    categories,
     lastUpdated,
     shouldUpdate,
-    accountTypes,
   };
 });
