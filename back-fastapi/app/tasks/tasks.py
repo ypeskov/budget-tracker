240a50e715e9c92fb7141ba1fc476b151c709c6f
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-05-18 11:07:41 +0300

    ignore celerybeat-schedule.db

--- back-fastapi/app/tasks/tasks.py
+++ back-fastapi/app/tasks/tasks.py
@@ -1,8 +1,7 @@
-from datetime import date
+from datetime import date, datetime
 
 from app.celery import celery_app
 from app.database import get_db
-from app.models.ExchangeRateHistory import ExchangeRateHistory
 from app.services.exchange_rates import update_exchange_rates as update_exchange_rates
 
 
@@ -12,10 +11,16 @@ def send_email():
     return "Email sent!"
 
 
-@celery_app.task
-def daily_update_exchange_rates():
+@celery_app.task(bind=True, max_retries=24, default_retry_delay=20)
+def daily_update_exchange_rates(task):
     db = next(get_db())
 
-    exchange_rates: ExchangeRateHistory = update_exchange_rates(db, when=date.today())
+    try:
+        update_exchange_rates(db, when=date.today())
+    except Exception as e:
+        raise task.retry(exc=e)
 
-    return exchange_rates
+    now = datetime.now()
+    print(f"Exchange rates updated at {now}")
+
+    return f"Exchange rates updated at {now}"
