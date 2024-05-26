979399172bdf3d5370e85af4853db62c17e48174
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-05-26 13:25:13 +0300

    version bump 0.41

--- back-fastapi/app/utils/db/backup.py
+++ back-fastapi/app/utils/db/backup.py
@@ -47,8 +47,8 @@ def backup_postgres_db(env_name: str,
 
         return True
     except subprocess.CalledProcessError as e:
-        logger.error(f"Error creating backup of DB [{dbname}]: {e}")
-        raise e
+        logger.error(f"Error creating backup of DB [{dbname}]: {e.stderr}")
+        # raise e
     except Exception as e:
-        logger.exception(e)
-        raise e
+        logger.error(e.strerror)
+        # raise e
