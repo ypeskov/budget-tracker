c16b4857c1dff8f25fa1e56817afa4814c7ef215
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-05-27 09:07:21 +0300

    add draft implementation of sending email after db backup

--- back-fastapi/app/routes/management.py
+++ back-fastapi/app/routes/management.py
@@ -1,12 +1,11 @@
 import os
 from pathlib import Path
 
-from fastapi import APIRouter, Depends, Request, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
 from icecream import ic
-from sqlalchemy.orm import Session
 
 from app.config import Settings
-from app.database import get_db
 from app.dependencies.check_token import check_token
 from app.logger_config import logger
 from app.utils.db.backup import backup_postgres_db
@@ -21,9 +20,37 @@ router = APIRouter(
     dependencies=[Depends(check_token)]
 )
 
+conf = ConnectionConfig(
+    MAIL_USERNAME="yuriy.peskov@gmail.com",
+    MAIL_PASSWORD="qdno gztk dgaa rxpv",
+    MAIL_FROM="yuriy.peskov@gmail.com",
+    MAIL_PORT=587,
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_FROM_NAME="OrgFin.run",
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    TEMPLATE_FOLDER=Path(os.getcwd()) / 'templates/emails',
+)
+
+
+async def simple_send():
+    message = MessageSchema(subject="Fastapi-Mail module",
+                            recipients=["yura@peskov.in.ua", ],
+                            template_body={
+                                "env_name": "dev",
+                                "db_name": settings.DB_NAME,
+                            },
+                            subtype=MessageType.html)
+
+    fm = FastMail(conf)
+    await fm.send_message(message, template_name="backup_created.html")
+
+    return True
+
 
 @router.get('/backup/')
-def backup_db(request: Request, db: Session = Depends(get_db)):
+async def backup_db():
     """ Create a backup of the database """
     logger.info('Backup of the database is requested')
     base_dir = Path(os.getcwd())
@@ -39,6 +66,9 @@ def backup_db(request: Request, db: Session = Depends(get_db)):
                            user=settings.DB_USER,
                            password=settings.DB_PASSWORD,
                            backup_dir=backup_dir)
+
+        await simple_send()
+
         return {'message': 'Backup of the database is successfully created'}
     except Exception as e:
         logger.exception(e)
