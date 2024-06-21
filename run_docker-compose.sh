cd42b309030f6b43917b0b278d2bd181a71441c9
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-06-21 15:44:40 +0300

    start docker with all services (except front)

--- run_docker-compose.sh
+++ run_docker-compose.sh
@@ -3,11 +3,13 @@
 if [ $# -eq 0 ]; then
   echo "Starting services without back app..."
   docker-compose up db-orgfin \
-                    redis-orgfin
-#                    dashboard-redis-celery
+                    redis-orgfin \
+                    api-orgfin \
+                    celery-orgfin \
+                    celery-beat-orgfin \
+                    dashboard-redis-celery
 #                    adminer-orgfin \
-#                    celery-orgfin \
-#                    celery-beat-orgfin \
+
 
 elif [ "$1" == "full" ]; then
   echo "Starting all services..."
