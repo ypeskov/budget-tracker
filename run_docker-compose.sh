3b97a151a95d3674dc7380a97c665d37037bc027
Yuriy Peskov <yuriy.peskov@gmail.com>
2024-07-17 09:48:25 +0300

    fix of deprecate warning in nom

--- run_docker-compose.sh
+++ run_docker-compose.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]; then
   echo "Starting services without back app..."
-  docker-compose up db-orgfin \
+  docker compose up db-orgfin \
                     redis-orgfin \
 #                    celery-orgfin \
 #                    celery-beat-orgfin \
@@ -13,9 +13,9 @@ if [ $# -eq 0 ]; then
 
 elif [ "$1" == "full" ]; then
   echo "Starting all services..."
-  docker-compose up
+  docker compose up
 elif [ "$1" == "prod" ]; then
-  docker-compose -f docker-compose.prod.yml up
+  docker compose -f docker-compose.prod.yml up
 else
   echo "Unknown argument: $1"
   echo "Variants:"
