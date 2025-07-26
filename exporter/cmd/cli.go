232a095031592ae30d7098df8f40c7d9d70260f6
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-07-26 21:15:56 +0300

    add CLI argument for .env file

--- exporter/cmd/cli.go
+++ exporter/cmd/cli.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"log/slog"
+	"os"
+
+	"github.com/spf13/cobra"
 
 	"orgfin.run/exporter/internal/config"
 	logger "orgfin.run/exporter/internal/logger"
 )
 
+var envFile string
+
+var rootCmd = &cobra.Command{
+	Use: "exporter",
+}
+
 func main() {
-	cfg, err := config.New("")
+	rootCmd.PersistentFlags().StringVarP(&envFile, "env", "e", ".env", "path to .env file")
+
+	if err := rootCmd.Execute(); err != nil {
+		slog.Error("cli failed", "error", err)
+		os.Exit(1)
+	}
+
+	cfg, err := config.New(envFile)
 	if err != nil {
 		slog.Error("Failed to load config", "error", err)
 		return
 	}
 
-	logger.Init("dev")
+	logger.Init(cfg.Env)
 
 	slog.Info("Config: ", slog.Any("config", cfg))
 }
--- exporter/go.mod
+++ exporter/go.mod
@@ -6,4 +6,10 @@ require (
 	github.com/caarlos0/env/v11 v11.3.1
 	github.com/joho/godotenv v1.5.1
 	github.com/lmittmann/tint v1.1.2
+	github.com/spf13/cobra v1.9.1
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.7 // indirect
 )
