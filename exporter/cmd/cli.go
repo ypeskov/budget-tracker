7b8957c0ddb733a2095dbfdd1492fccfb4da0181
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-07-27 08:52:48 +0300

    some refactoring and clean up

--- exporter/cmd/cli.go
+++ exporter/cmd/cli.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"log/slog"
+	"os"
 	"strconv"
 
 	"orgfin.run/exporter/internal/config"
 	"orgfin.run/exporter/internal/database"
 	logger "orgfin.run/exporter/internal/logger"
+	"orgfin.run/exporter/internal/models"
 	"orgfin.run/exporter/internal/respositories"
 	"orgfin.run/exporter/internal/services"
 )
@@ -21,7 +23,7 @@ func main() {
 	cfg, err := config.New(*envFile)
 	if err != nil {
 		slog.Error("Failed to load config", "error", err)
-		return
+		os.Exit(1)
 	}
 
 	logger.Init(cfg)
@@ -29,38 +31,48 @@ func main() {
 	db, err := database.New(cfg)
 	if err != nil {
 		slog.Error("Failed to connect to database", "error", err)
-		return
+		os.Exit(1)
 	}
 
 	slog.Info("App initialized")
 
-	run(db, *userID)
+	if err := run(db, *userID); err != nil {
+		slog.Error("Failed to run app", "error", err)
+		os.Exit(1)
+	}
 
-	slog.Info("App finished")
+	slog.Info("App finished successfully")
 }
 
 // The main function that runs the app.
 // It gets the transactions for the user and exports them to a CSV file.
-func run(db *database.Database, userID string) {
+func run(db *database.Database, userID string) error {
 	slog.Info("Running app...")
 
 	userIDInt, err := strconv.ParseInt(userID, 10, 64)
 	if err != nil {
 		slog.Error("Failed to parse user id", "error", err)
-		return
+		return err
 	}
 
 	slog.Info("Getting transactions...")
 	transactionsRepository := respositories.NewTransactionsRepository(db)
 	transactionsService := services.NewTransactionsService(transactionsRepository)
 
-	transactions, err := transactionsService.GetAllForUser(userIDInt)
-	if err != nil {
+	var transactions []models.Transaction
+	if transactions, err = transactionsService.GetAllForUser(userIDInt); err != nil {
 		slog.Error("Failed to get all transactions for user", "error", err)
-		return
+		return err
 	}
+
 	slog.Info("Transactions fetched", "count", len(transactions))
 
 	csvExporter := services.NewCSVExporter(transactions)
-	csvExporter.ExportToCSV(transactions)
+	if err := csvExporter.ExportToCSV(transactions); err != nil {
+		slog.Error("Failed to export transactions to csv", "error", err)
+		return err
+	}
+
+	slog.Info("Transactions exported to csv")
+	return nil
 }
