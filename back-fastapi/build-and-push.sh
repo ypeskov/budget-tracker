8d56ef275415a36a1b5e4721299431173b91704c
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-08-06 08:26:24 +0300

    updated build and push script

---
+++ back-fastapi/build-and-push.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+get_tag() {
+    local tag="latest"
+
+    for arg in "$@"; do
+        if [[ $arg != --platform=* ]] && [[ $arg != "push" ]]; then
+            tag=$arg
+            break
+        fi
+    done
+
+    echo "$tag"
+}
+
+build_and_tag() {
+    local tag=$1
+    local platform_option="--platform=linux/arm64"
+
+    local build_command="docker build --target prod "
+    
+    build_command+="--no-cache $platform_option"
+    build_command+=" -t ypeskov/api-orgfin:$tag ."
+    echo "$build_command"
+
+    eval "$build_command"
+    echo "$tag" > version.txt
+}
+
+update_kubernetes_deployment() {
+    local tag=$1
+
+    # edit kubernetes deployment files
+    sed -i "" "s|ypeskov/api-orgfin:[^[:space:]]*|ypeskov/api-orgfin:${tag}|g" ../Kubernetes/orgfin-backend/base/deployment-api.yaml
+    sed -i "" "s|ypeskov/api-orgfin:[^[:space:]]*|ypeskov/api-orgfin:${tag}|g" ../Kubernetes/orgfin-backend/base/deployment-celery.yaml
+    sed -i "" "s|ypeskov/api-orgfin:[^[:space:]]*|ypeskov/api-orgfin:${tag}|g" ../Kubernetes/orgfin-backend/base/deployment-beat.yaml
+}
+
+if [[ $# -eq 0 ]]; then
+    tag=$(get_tag)
+    build_and_tag $tag
+elif [[ $1 == "push" ]]; then
+    tag=$(get_tag "${@:2}")
+    build_and_tag $tag
+    update_kubernetes_deployment $tag
+    docker push ypeskov/api-orgfin:$tag
+
+    echo "$tag" > ../version.txt
+else
+    echo "Usage:"
+    echo "$0                                           # Build and tag :latest"
+    echo "$0 push                                      # Build, tag and push :latest to Docker Hub"
+    echo "$0 push <tag>                                # Build, tag as <tag> and push to Docker Hub"
+    echo "$0 push [--platform=<platform>]              # Build for optional platform, tag as :latest, and push to Docker Hub"
+    echo "$0 push <tag> [--platform=<platform>]        # Build for optional platform, tag as <tag>, and push to Docker Hub"
+fi
