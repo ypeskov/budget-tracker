cf93772eecfc5536adcb7fac84fdf6b600c62717
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-08-08 09:21:05 +0300

    qqq

--- back-fastapi/app/schemas/token_schema.py
+++ back-fastapi/app/schemas/token_schema.py
@@ -4,8 +4,6 @@ from pydantic.alias_generators import to_camel
 
 class Token(BaseModel):
     access_token: str
-    token_type: str
+    token_type: str = "bearer"
 
-    model_config = ConfigDict(from_attributes=True,
-                              populate_by_name=True,
-                              alias_generator=to_camel)
+    model_config = ConfigDict(from_attributes=True, populate_by_name=True, alias_generator=to_camel)
