f359cdfde55ac863e10502bc0924b0c5785198ab
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-08-08 18:19:48 +0300

    qqq

--- back-fastapi/app/schemas/account_schema.py
+++ back-fastapi/app/schemas/account_schema.py
@@ -13,15 +13,13 @@ class CreateAccountSchema(BaseModel):
     user_id: int | None = None
     account_type_id: int
     currency_id: int
-    initial_balance: Annotated[Decimal, PlainSerializer(
-            lambda x: float(x), return_type=float, when_used='json'
-        )] = Decimal(0)
-    balance: Annotated[Decimal, PlainSerializer(
-            lambda x: float(x), return_type=float, when_used='json'
-        )] = Decimal(0)
-    credit_limit: Annotated[Decimal, PlainSerializer(
-            lambda x: float(x), return_type=float, when_used='json'
-        )] = Decimal(0)
+    initial_balance: Annotated[Decimal, PlainSerializer(lambda x: float(x), return_type=float, when_used='json')] = (
+        Decimal(0)
+    )
+    balance: Annotated[Decimal, PlainSerializer(lambda x: float(x), return_type=float, when_used='json')] = Decimal(0)
+    credit_limit: Annotated[Decimal, PlainSerializer(lambda x: float(x), return_type=float, when_used='json')] = (
+        Decimal(0)
+    )
     name: str
     opening_date: datetime | None = None
     comment: str = ""
@@ -34,23 +32,18 @@ class CreateAccountSchema(BaseModel):
             return Decimal(0)
         return value
 
-    model_config = ConfigDict(populate_by_name=True,
-                              alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
 
 class UpdateAccountSchema(CreateAccountSchema):
     id: int | None = None
-    balance: Annotated[Decimal, PlainSerializer(
-            lambda x: float(x), return_type=float, when_used='json'
-        )]
+    balance: Annotated[Decimal, PlainSerializer(lambda x: float(x), return_type=float, when_used='json')]
     opening_date: datetime
     comment: str
     is_hidden: bool
     show_in_reports: bool
 
-    model_config = ConfigDict(populate_by_name=True,
-                              from_attributes=True,
-                              alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True, from_attributes=True, alias_generator=to_camel)
 
 
 class AccountResponseSchema(CreateAccountSchema):
@@ -59,18 +52,15 @@ class AccountResponseSchema(CreateAccountSchema):
     account_type: AccountTypeResponseSchema
     is_deleted: bool
     is_archived: bool
-    balance_in_base_currency: Annotated[Decimal, PlainSerializer(
-        lambda x: float(x or 0), return_type=float, when_used='json'
-    )] = None
+    balance_in_base_currency: Annotated[
+        Decimal, PlainSerializer(lambda x: float(x or 0), return_type=float, when_used='json')
+    ] = None
     archived_at: datetime | None = None
-    model_config = ConfigDict(from_attributes=True,
-                              populate_by_name=True,
-                              alias_generator=to_camel)
+    model_config = ConfigDict(from_attributes=True, populate_by_name=True, alias_generator=to_camel)
 
 
 class AccountArchiveStatusSchema(BaseModel):
     account_id: int
     is_archived: bool
 
-    model_config = ConfigDict(populate_by_name=True,
-                              alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
