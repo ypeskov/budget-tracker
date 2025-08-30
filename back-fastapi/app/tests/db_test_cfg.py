f936b1c3e9856cf5c565a3f988eec1268aa24e28
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-08-30 19:03:11 +0300

    fix start of tests

--- back-fastapi/app/tests/db_test_cfg.py
+++ back-fastapi/app/tests/db_test_cfg.py
@@ -5,11 +5,11 @@ from app.database import Base
 
 DB_NAME = 'budgeter_test'
 DB_USER = 'postgres'
-DB_PASSWORD = 'budgeter'
+DB_PASSWORD = '123'
 # DB_HOST = 'db-budgeter'
 DB_HOST = 'localhost'
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
