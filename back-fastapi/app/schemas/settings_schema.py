be3932fef6ace4cfeb5ef094f94af057d9ad4bcb
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-10-20 17:00:11 +0300

    fix base currency change

--- back-fastapi/app/schemas/settings_schema.py
+++ back-fastapi/app/schemas/settings_schema.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 
 class BaseCurrencyInputSchema(BaseModel):
     currency_id: int
 
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True, alias_generator=to_camel)
 
 
 class UserSettingsSchema(BaseModel):
